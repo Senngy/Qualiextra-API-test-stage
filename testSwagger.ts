f7ab766aac6ad58472634ad2077ade2f8223fa28
Senngy <sylvainmarchal@hotmail.com>
2025-09-05 13:58:08 +0200

    Delete test swagger file

--- testSwagger.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import swaggerUi from 'swagger-ui-express';
-import swaggerDocument from './build/swagger/swagger.json';
-
-const app = express();
-
-app.use('/swagger-ui', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-app.listen(3001, () => console.log('http://localhost:3001/swagger-ui'));
